2654bc7cd5c79c9dfe5c8e2afe74e466315906c1
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-04-12 09:39:21 -0600

    Smurf gettening began

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,34 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+
+import axios from 'axios';
+export const GET_SMURFS = 'GET_SMURFS';
+export const SMURFS_GOT = 'SMURFS_GOT';
+export const ADD_SMURF  = 'ADD_SMURF';
+export const SMURF_ADDED = 'SMURF_ADDED';
+export const ERROR ='ERROR';
+
+
+const url = 'http://localhost:3333/'
+
+export const getSmurfs = () => dispatch => {
+  dispatch({
+    type: GET_SMURFS
+  })
+  axios
+  .get(`${url}smurfs`)
+  .then(response => {
+    dispatch({
+      type: SMURFS_GOT, 
+      payload: response.data
+    })
+  })
+  .catch(err => {
+    dispatch({
+      type: ERROR,
+      payload: err
+    })
+  })
+}
